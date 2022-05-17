e55105aa6cfd9e064876dd25d678c3e196d7e058
Dryspell <Nhwinslow@gmail.com>
2022-05-17 11:43:16 -0400

    Followed up to 4

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is working");
+});
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res) => {
+    const chat = chats.find((chat) => chat._id === req.params.id);
+    if (chat) {
+        res.send(chat);
+    } else {
+        res.status(404).send({ message: "Chat not found" });
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
