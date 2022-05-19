8c58f3394896ce9b40daae16002ab5891bd9d802
Dryspell <Nhwinslow@gmail.com>
2022-05-18 21:59:03 -0400

    Backend Mongoose Models done 6

---
+++ backend/models/messageModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        sender: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        chat: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Chat",
+        },
+        message: {
+            type: String,
+            required: true,
+        },
+        isRead: {
+            type: Boolean,
+            default: false,
+        },
+        isDeleted: {
+            type: Boolean,
+            default: false,
+        },
+        isEdited: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const Message = mongoose.model("Message", messageModel);
+
+module.exports = Message;
