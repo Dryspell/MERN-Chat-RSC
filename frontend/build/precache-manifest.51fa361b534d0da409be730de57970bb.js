36c3515cb7a574a2501ec1336fa2e3d1921daa40
Dryspell <Nhwinslow@gmail.com>
2022-06-12 13:31:06 -0400

    rebuild

---
+++ frontend/build/precache-manifest.51fa361b534d0da409be730de57970bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6a43452f53ba388bcc5",
+    "url": "/static/css/main.c3545312.chunk.css"
+  },
+  {
+    "revision": "b6a43452f53ba388bcc5",
+    "url": "/static/js/main.0025d5ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55a2d8c3015f50282046",
+    "url": "/static/js/2.5572e9e1.chunk.js"
+  },
+  {
+    "revision": "d22141fb5a7c016357bbb40a9aff22ba",
+    "url": "/static/media/background.d22141fb.jpg"
+  },
+  {
+    "revision": "fe4340d48d7c62ed64a615d0a9b3102a",
+    "url": "/index.html"
+  }
+];
