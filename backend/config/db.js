0afbe111c26ee171c51e3a3fd5b598c0b6e55f58
Dryspell <Nhwinslow@gmail.com>
2022-06-12 10:39:16 -0400

    changed mongo connection string

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,13 @@ const color = require("colors");
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const connection = await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_URI}`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
         console.log(
             `MongoDB Connected to ${connection.connection.host}`.cyan.underline
                 .bold
