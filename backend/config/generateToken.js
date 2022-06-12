d20af707541b604a068eb6610731a42573bcf536
Dryspell <Nhwinslow@gmail.com>
2022-06-12 12:19:38 -0400

    Token generation failure?

--- backend/config/generateToken.js
+++ backend/config/generateToken.js
@@ -1,9 +1,14 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN,
-    });
+    console.log(`Generating token for user: ${id}`);
+    try {
+        return jwt.sign({ id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        });
+    } catch (error) {
+        console.log(`Error generating token: ${error}`);
+    }
 };
 
 module.exports = { generateToken };
